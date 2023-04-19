6907ddd0b8a742fa63c2967dc52ec4ba90a33e16
TTAN20 <75953370+TTAN20@users.noreply.github.com>
2023-04-19 02:34:22 -0400

    Create Huffman_sequential.cpp

---
+++ Huffman_sequential.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+
+struct node {
+ 
+    // One of the input characters
+    char data;
+ 
+    // Frequency of the character
+    unsigned freq;
+    // Left and right child of this node
+    struct node *left;
+    struct node *right;
+};
+
+struct tree {
+     
+    unsigned size;
+    unsigned capacity;
+    struct node **array;
+};
+
+struct tree *newNode(char data, unsigned frequency) {
+    struct node *result = (struct node*)malloc(sizeof(struct node));
+    result->left = NULL;
+    result->right = NULL;
+    result->data = data;
+    result->freq = frequency;
+    return result;
+}
+
+struct tree *newTree(unsigned capacity) {
+    struct tree *result = (struct tree*)malloc(sizeof(struct tree));
+    result->size = 0;
+    result->capacity = capacity;
+    result->array = (struct node**)malloc(result->capacity * sizeof(struct node*));
+    return result
+}
+
+void swapNode(struct node** first, struct node** second) {
+    struct node* temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+void insertNode(struct tree* bTree, struct node* bNode) {
+    int index = bTree->size - 1;
+    while (index && bNode->freq < bTree->array[(index - 1)/2]->freq) {
+        index = (index - 1) / 2;
+    } 
+    bTree->array[index] = bNode;
+    bTree->size = bTree->size + 1;
+}
+
+void buildHtree(struct tree* bTree, int index) {
+    int least = index;
+    int left = 2 * index + 1;
+    int right = left + 1;
+    bool cond1 = left < bTree->size;
+    bool cond2 = bTree->array[left]->freq < bTree->array[least]->freq;
+    if (cond1 && cond2) {
+        least = left;
+    }
+    bool cond3 = right < bTree->size;
+    bool cond4 = bTree->array[right]->freq < bTree->array[least]->freq;
+    if (cond3 && cond4) {
+        least = right;
+    }
+    if (least != index) {
+        swapNode(&bTree->array[least], &bTree->array[index]);
+        buildHtree(bTree, least);
+    }
+}
+
+struct node* getMin(struct tree* bTree) {
+    int last = bTree->size - 1;
+    struct node* result = bTree->array[0];
+    bTree->array[0] = last;
+    bTree->size = bTree->size - 1;
+    createHtree(bTree, 0);
+    return result;
+}
+
+void createHTree(struct tree* bTree) {
+    int max = bTree->size - 1;
+    for (int i = (max - 1)/2; i >= 0; i--) {
+        buildHtree(bTree, i);
+    }
+}
+
