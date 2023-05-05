275e79bca0995674e754ca00ab4683807bd53eaa
alaffont <55415433+alaffont@users.noreply.github.com>
2023-05-05 10:42:09 -0400

    Update LZSS_parallel.cu

--- LZSS_parallel.cu
+++ LZSS_parallel.cu
@@ -32,6 +32,7 @@ using namespace std;
 // GLOBAL VARIABLES
 #define MATCH_LENGTH_MASK (15)
 #define WINDOW_SIZE (4095)
+#define GRANULARITY (WINDOW_SIZE+1)
 
 // ascii to char
 void print_char(int v){
@@ -72,8 +73,6 @@ bool are_vectors_equal(std::vector<unsigned char> s1, std::vector<unsigned char>
 
 __global__ void find_longest_match_kernel(unsigned char *data, int i, uint16_t *longest_match_array, int len) {
     
-    __shared__ bool terminate_early = false;
-
     uint orig_i = i;
     uint cur = threadIdx.x + max(0, i - WINDOW_SIZE);
     uint16_t orig_cur = cur;
@@ -81,20 +80,15 @@ __global__ void find_longest_match_kernel(unsigned char *data, int i, uint16_t *
     uint length_of_match = 0;
     uint longest_match = 0;
     uint start = 0;
-    // printf("thread idx: %d, cur: %d i: %d\n", threadIdx.x, cur, i);
     while(cur < i && i < len && length_of_match < MATCH_LENGTH_MASK){
         // we have a match
-        // printf("cur data: %c\n", data[cur]);
-        // printf("i data: %c\n", data[i]);
         if(data[cur] == data[i]){
-            // printf("equal!\n");
             cur++;
             i++;
             length_of_match++;
         }else{
             // we have a new longest match
             if (longest_match < length_of_match){
-                // printf("new longest match!\n");
                 longest_match = length_of_match;
                 length_of_match = 0;
                 start = cur;
@@ -102,18 +96,24 @@ __global__ void find_longest_match_kernel(unsigned char *data, int i, uint16_t *
             break;
         }
     }
-    if (longest_match <= 2){
-        // printf("return 0\n");
+    // dont compress size 0 or 1
+    if (longest_match < 2){
         longest_match_array[orig_cur] = 0;
-    }else{
-        // printf("return not 0\n");
+    }
+    // compress case
+    else{
         uint16_t ret = 0;
+        // bit shifting based on header of file
         ret = ((orig_i - ((start - longest_match))) << 4) | (longest_match);
         longest_match_array[orig_cur] = ret;
     }
 }
 
-__global__ void Max_Interleaved_Addressing_Global(uint16_t* data, int data_size){
+// use a reduction function to find the max value in an array
+// set the first element to this max
+// we only want to base our max on size rather than distance
+__global__ void find_max_reduction_kernel(uint16_t* data, int data_size){
+    // we launch one thread per index
     int idx = threadIdx.x;
     if (idx < data_size){
         for(int stride = 1; stride < data_size; stride *= 2) {
@@ -127,6 +127,48 @@ __global__ void Max_Interleaved_Addressing_Global(uint16_t* data, int data_size)
     }
 }
 
+uint16_t seq_find_longest_match(std::vector<unsigned char> data, int i){
+    uint orig_i = i;
+    uint cur = max(0, i - WINDOW_SIZE);
+    // create the struct for storing the longest word we are currently look at
+    uint length_of_match = 0;
+    uint longest_match = 0;
+    uint start = 0;
+    while(cur < i && i < data.size()){
+        if(data.at(i) == data.at(cur) && length_of_match <= MATCH_LENGTH_MASK){
+            // increment i and cur since we have a match
+            i++;
+            cur++;
+            length_of_match++;
+        }else{
+            // check if longest string so far
+            if (longest_match < length_of_match){
+                // new length
+                start = cur;
+                longest_match = length_of_match;
+                // break early if we find max length match
+                if (longest_match >= MATCH_LENGTH_MASK)
+                    break;
+            }
+            // reset length of current match 
+            length_of_match = 0;
+            cur += 1;
+            i = orig_i;
+        }
+    } 
+
+    // 12 bytes = distance and 4 bytes is length
+    if(longest_match < 2){
+        return 0;
+    }
+
+    uint16_t ret = 0;
+    // use our custom way of storing length and start of match in 2 bytes
+    ret = ((orig_i - ((start - longest_match))) << 4) | (longest_match);
+
+    return ret;
+}
+
 
 // keep an array of longest matches. 
 // have each index select the length of the match  
@@ -138,17 +180,19 @@ uint16_t find_longest_match(unsigned char *d_data, int input_size, int i){
     // set things to 0 if not useful so no need to worry
     cudaMalloc(&device_longest_match_array, 2 * longest_match_array_length);
     // invoke our kernel
-    find_longest_match_kernel<<<1, min(i + 1, number_of_threads)>>>(d_data, i, device_longest_match_array, input_size);
+    find_longest_match_kernel<<<1, min(i+1, number_of_threads)>>>(d_data, i, device_longest_match_array, input_size);
     // find max in using interleave
-    Max_Interleaved_Addressing_Global<<<1, min(i + 1, number_of_threads)>>>(device_longest_match_array, input_size);
+    find_max_reduction_kernel<<<1, min(i+1, number_of_threads)>>>(device_longest_match_array, input_size);
     // alloc host mem
-    uint16_t *host_longest_match_array = (uint16_t*)malloc(2);
+    uint16_t res;
+    cudaMemcpy(&res, device_longest_match_array, 2, cudaMemcpyDeviceToHost);
+
     // only need to copy one item
-    cudaMemcpy(host_longest_match_array, device_longest_match_array, 2, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(host_longest_match_array, device_longest_match_array, 2*longest_match_array_length, cudaMemcpyDeviceToHost);
 
-    cudaFree(device_longest_match_array);
+    // cudaFree(device_longest_match_array);
 
-    return host_longest_match_array[0];
+    return res;
 }
 
 
@@ -158,13 +202,19 @@ std::vector<unsigned char> compress(unsigned char *d_data, std::vector<unsigned 
     std::vector<unsigned char> output_buffer;
     // loop through all char in text
     while (i < input_size){
-        uint16_t longest_match = find_longest_match(d_data, input_size, i);
+        uint16_t longest_match;
+        // granularity tells us if we should run code in parallel or sequentially
+        if (i > GRANULARITY){
+            longest_match = seq_find_longest_match(data, i);
+        }else{
+            longest_match = find_longest_match(d_data, input_size, i);
+        }
         uint16_t match_length = get_length(longest_match); 
         // item needs to be longer than 2 in order to save data
         if(match_length > 2){
             // 12 bytes = distance and 4 bytes is length
             uint16_t match_distance = get_distance(longest_match);
-            uint16_t match_length = get_length(longest_match); 
+            // uint16_t match_length = get_length(longest_match); 
             // indicate that we are compressing
             compress_flags[i] = 1;
             output_buffer.push_back((longest_match >> 8) & 0xFF);
@@ -179,6 +229,8 @@ std::vector<unsigned char> compress(unsigned char *d_data, std::vector<unsigned 
     return output_buffer;
 }
 
+// decompress unsigned char vector streams 
+// actual structure of data compression is described in header
 std::vector<unsigned char> decompress(std::vector<unsigned char> data, std::vector<bool> &compress_flags){
     int compress_index = 0;
     uint data_index = 0;
@@ -205,30 +257,21 @@ std::vector<unsigned char> decompress(std::vector<unsigned char> data, std::vect
         else{
             output.push_back(data[data_index]);
             // one byte for non compressed
-            data_index += 1;
-            compress_index += 1;
+            data_index++;
+            compress_index++;
         }
     }
     return output;
 }
 
-__global__ void print_kernel() {
-    printf("Hello from block %d thread idx: %d\n", blockIdx.x,threadIdx.x);
-}
-
-
 int main(int argc, char *argv[]) {
-    // printf("%d",argc);
-    // printf("%s", argv[1]);
     std::ifstream inFile;
-    //open the input file
     inFile.open(argv[1]); 
     
     std::stringstream strStream;
     auto read_file_start_time = std::chrono::high_resolution_clock::now();
     //read the file
     strStream << inFile.rdbuf(); 
-    //str holds the content of the file
     std::string s = strStream.str(); 
     // text input
     vector<unsigned char> input(s.begin(), s.end());
@@ -240,7 +283,6 @@ int main(int argc, char *argv[]) {
     cudaMemcpy(d_data, input.data(), input_size, cudaMemcpyHostToDevice);
 
     auto read_file_end_time = std::chrono::high_resolution_clock::now();
-
 
     // compression flags all init at 0
     vector<bool> compress_flags(input.size(), 0);
@@ -254,9 +296,10 @@ int main(int argc, char *argv[]) {
     auto output = decompress(compressed_buf, compress_flags);
     auto decompression_end_time = std::chrono::high_resolution_clock::now();
 
-
+    // verify results
     bool res = are_vectors_equal(output, input);
 
+    // save time durations
     auto compression_duration = (std::chrono::duration_cast<std::chrono::microseconds>(compression_end_time - compression_start_time).count()/pow(10,6));
     auto decompression_duration = (std::chrono::duration_cast<std::chrono::microseconds>(decompression_end_time - decompression_start_time).count()/pow(10,6));
     auto read_duration = (std::chrono::duration_cast<std::chrono::microseconds>(read_file_end_time - read_file_start_time).count()/pow(10,6));
@@ -265,10 +308,11 @@ int main(int argc, char *argv[]) {
     ofstream myfile;
     myfile.open("output.txt");
 
+    // report duartion inside outfile
     myfile << "Reading Input & Copying to GPU took: " << read_duration << " seconds" << std::endl;
     myfile << "Compression took: " << compression_duration << " seconds" << std::endl;
     myfile << "Decompression took: " << decompression_duration << " seconds" << std::endl;
     myfile.close();
-
+    
     return 1;
 }
